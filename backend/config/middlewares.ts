8626cbaf796ee8d004c14f26d6db720eae346c17
Ronaldo Almirón <rony.almiron.015@gmail.com>
2026-01-12 12:43:59 -0300

    Agregar configuración de política de seguridad de contenido y reordenar middlewares; eliminar opciones de acción en el plugin de Cloudinary

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,12 +1,35 @@
 export default [
-  'strapi::logger',
   'strapi::errors',
   'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://res.cloudinary.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://res.cloudinary.com',
+          ],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
+
