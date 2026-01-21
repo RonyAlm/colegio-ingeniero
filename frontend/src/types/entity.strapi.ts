dd40520420dca070b91498021e4467c221945e35
Ronaldo Almirón <rony.almiron.015@gmail.com>
2026-01-21 10:42:53 -0300

    Agregar tipos de datos para la estructura del sitio en entity.strapi y exportarlos desde index

---
+++ frontend/src/types/entity.strapi.ts
@@ -0,0 +1,255 @@
+// {
+//     "data": {
+//         "id": 1,
+//         "documentId": "sbqzif7i4ioxuab5erqu07dc",
+//         "siteName": "Colegio Público de Ingenieros de Formosa",
+//         "siteDescription": "Este es el sitio oficial del Colegio Público de Ingenieros de Formosa",
+//         "createdAt": "2026-01-09T11:42:02.137Z",
+//         "updatedAt": "2026-01-15T12:36:43.252Z",
+//         "publishedAt": "2026-01-15T12:36:35.066Z",
+//         "favicon": {
+//             "id": 15,
+//             "documentId": "swsslt4x9wwr34f05ezh7jlu",
+//             "url": "https://res.cloudinary.com/du86bx5o0/image/upload/v1768232569/logo_c033048d58.png",
+//             "alternativeText": null
+//         },
+//         "defaultSeo": {
+//             "id": 1,
+//             "metaTitle": "Colegio Público de Ingenieros de Formosa",
+//             "metaDescription": "Este es el sitio oficial del Colegio Público de Ingenieros de Formosa"
+//         },
+//         "header": {
+//             "id": 1,
+//             "logo": {
+//                 "id": 1,
+//                 "label": "Colegio Público de Ingenieros de Formosa",
+//                 "href": "/",
+//                 "isExternal": false,
+//                 "icon": null,
+//                 "image": {
+//                     "id": 15,
+//                     "documentId": "swsslt4x9wwr34f05ezh7jlu",
+//                     "url": "https://res.cloudinary.com/du86bx5o0/image/upload/v1768232569/logo_c033048d58.png",
+//                     "alternativeText": null
+//                 }
+//             },
+//             "navItems": [
+//                 {
+//                     "id": 35,
+//                     "href": "/institucionales",
+//                     "label": "Institucionales",
+//                     "isExternal": false,
+//                     "isButtonLink": false,
+//                     "type": null
+//                 },
+//                 {
+//                     "id": 36,
+//                     "href": "/servicios",
+//                     "label": "Servicios",
+//                     "isExternal": false,
+//                     "isButtonLink": false,
+//                     "type": null
+//                 },
+//                 {
+//                     "id": 37,
+//                     "href": "/novedades",
+//                     "label": "Novedades",
+//                     "isExternal": false,
+//                     "isButtonLink": false,
+//                     "type": null
+//                 },
+//                 {
+//                     "id": 38,
+//                     "href": "/contacto",
+//                     "label": "Contacto",
+//                     "isExternal": false,
+//                     "isButtonLink": false,
+//                     "type": null
+//                 }
+//             ],
+//             "cta": [
+//                 {
+//                     "id": 39,
+//                     "href": "https://intranet.ingenierosformosa.com.ar/",
+//                     "label": "Matriculados",
+//                     "isExternal": true,
+//                     "isButtonLink": true,
+//                     "type": "PRIMARY"
+//                 },
+//                 {
+//                     "id": 40,
+//                     "href": "https://darkgoldenrod-chinchilla-280670.hostingersite.com",
+//                     "label": "IngLink",
+//                     "isExternal": true,
+//                     "isButtonLink": true,
+//                     "type": "SECONDARY"
+//                 }
+//             ]
+//         },
+//         "footer": {
+//             "id": 1,
+//             "text": "Colegio Público de Ingenieros de Formosa. Todos los derechos reservados.",
+//             "logo": {
+//                 "id": 2,
+//                 "label": "Colegio Público de Ingenieros de Formosa",
+//                 "href": "/",
+//                 "isExternal": false,
+//                 "icon": null,
+//                 "image": {
+//                     "id": 14,
+//                     "documentId": "ab1ji1e24sosdx63gx3hs6py",
+//                     "url": "https://res.cloudinary.com/du86bx5o0/image/upload/v1768231863/logo_2_2283e1492c.png",
+//                     "alternativeText": null
+//                 }
+//             },
+//             "navItems": [
+//                 {
+//                     "id": 41,
+//                     "href": "/",
+//                     "label": "Contacto",
+//                     "isExternal": false,
+//                     "isButtonLink": false,
+//                     "type": null
+//                 }
+//             ],
+//             "socialLinks": [
+//                 {
+//                     "id": 3,
+//                     "label": null,
+//                     "href": "/instagram",
+//                     "isExternal": false,
+//                     "icon": "instagram",
+//                     "image": {
+//                         "id": 17,
+//                         "documentId": "oz5cnvlkbyfumodhdecx1xgt",
+//                         "url": "https://res.cloudinary.com/du86bx5o0/image/upload/v1768308464/instagram_7ab6517ed1.svg",
+//                         "alternativeText": null
+//                     }
+//                 },
+//                 {
+//                     "id": 4,
+//                     "label": null,
+//                     "href": "/facebook",
+//                     "isExternal": false,
+//                     "icon": "facebook",
+//                     "image": {
+//                         "id": 20,
+//                         "documentId": "yjf36oyjxioxilji8xrd2wpl",
+//                         "url": "https://res.cloudinary.com/du86bx5o0/image/upload/v1768308465/facebook_2681c2c588.svg",
+//                         "alternativeText": null
+//                     }
+//                 },
+//                 {
+//                     "id": 5,
+//                     "label": null,
+//                     "href": "#",
+//                     "isExternal": false,
+//                     "icon": "youtube",
+//                     "image": {
+//                         "id": 21,
+//                         "documentId": "sd92rsqbdkcz6i9eczqry4sv",
+//                         "url": "https://res.cloudinary.com/du86bx5o0/image/upload/v1768308465/youtube_c34261683d.svg",
+//                         "alternativeText": null
+//                     }
+//                 }
+//             ],
+//             "infoLinks": [
+//                 {
+//                     "id": 6,
+//                     "label": "Sede Social:  Av. Gutnisky N 1870",
+//                     "href": "#",
+//                     "isExternal": false,
+//                     "icon": "map",
+//                     "image": null
+//                 },
+//                 {
+//                     "id": 7,
+//                     "label": "Sede Administrativa: Av. 9 de Julio N 498",
+//                     "href": "/",
+//                     "isExternal": false,
+//                     "icon": "map",
+//                     "image": null
+//                 },
+//                 {
+//                     "id": 11,
+//                     "label": "Horarios: Lunes a Viernes de 8 a 12:30hs",
+//                     "href": "#",
+//                     "isExternal": false,
+//                     "icon": "hours",
+//                     "image": null
+//                 }
+//             ],
+//             "contactLinks": [
+//                 {
+//                     "id": 12,
+//                     "label": "ingenierosformosa@gmail.com",
+//                     "href": "#",
+//                     "isExternal": true,
+//                     "icon": "email",
+//                     "image": null
+//                 },
+//                 {
+//                     "id": 13,
+//                     "label": "WhatsApp: 3705 043114",
+//                     "href": "#",
+//                     "isExternal": true,
+//                     "icon": "whatsapp",
+//                     "image": null
+//                 },
+//                 {
+//                     "id": 14,
+//                     "label": "Fijo: 3705 383748",
+//                     "href": "#",
+//                     "isExternal": true,
+//                     "icon": "phone",
+//                     "image": null
+//                 }
+//             ]
+//         }
+//     },
+//     "meta": {}
+// }
+
+export interface TImagen {
+  id: string;
+  alternativeText: string | null;
+  ducumentId: string;
+  url: string;
+}
+
+export interface TLink {
+  id: number;
+  label: string;
+  href: string;
+  isExternal: boolean;
+  isButtonLink: boolean;
+  type: string | null;
+  icon: string;
+  image: TImagen;
+}
+
+export interface TLogoLink {
+  id: number;
+  label: string;
+  href: string;
+  isExternal: boolean;
+  icon: string;
+  image: TImagen;
+}
+
+export interface THeader {
+  id: number;
+  logo: TLogoLink;
+  navItems: TLink[];
+  cta: TLink;
+}
+
+export interface TFooter {
+  id: number;
+  text: string;
+  logo: TLogoLink;
+  navItems: TLink[];
+  socialLinks: TLink[];
+  infoLinks: TLink[];
+  contactLinks: TLink[];
+}
