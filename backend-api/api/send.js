53306b437829a6e8196dfbb177ce9a7e9967b893
Ronaldo Almirón <rony.almiron.015@gmail.com>
2026-04-06 08:47:36 -0300

    configurar CORS para permitir solo el dominio especificado

--- backend-api/api/send.js
+++ backend-api/api/send.js
@@ -11,9 +11,7 @@ const rateLimit = new Map()
 
 export default async function handler(req, res) {
 
-    // permitir CORS solo a tu dominio
-    // res.setHeader("Access-Control-Allow-Origin", "https://tudominio.com")
-    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Origin", URL_FRONTEND)
     res.setHeader("Access-Control-Allow-Methods", "POST")
     res.setHeader("Access-Control-Allow-Headers", "Content-Type")
 
