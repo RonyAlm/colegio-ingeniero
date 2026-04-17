691a958ddbabd90a77ca83d507dbc805086fbdda
Ronaldo Almirón <rony.almiron.015@gmail.com>
2026-04-17 12:36:53 -0300

    cambiar el orden de los artículos de publicado a creado en la consulta

--- backend-api/api/articles.js
+++ backend-api/api/articles.js
@@ -102,7 +102,7 @@ export default async function handler(req, res) {
 
                 WHERE a.published_at IS NOT NULL
 
-                ORDER BY a.published_at DESC
+                ORDER BY a.created_at DESC
                 LIMIT ? OFFSET ?
             `,
             [pageSize, offset]
